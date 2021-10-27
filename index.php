183fa00b5bc6e09235d7add197fab361f28fe1d1
JohnHajjar <john.hajjar@isae.edu.lb>
2021-10-27 15:04:49 +0300

    Section 1 Co-authored-by: ElieJ <ElieJ@users.noreply.github.com>

--- index.php
+++ index.php
@@ -4,12 +4,62 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>    -->
+        <script type="text/javascript" src="jquery-comp-3.6.js"></script>
     </head>
     <body>
 
+    <nav> 
+    </nav>
+    <div class="s1-container ">  <!-- container-flex -->
+        <div class = "text-Box">
+            <!-- <br><br><br><br> -->
+            <div class = "centered s1-1 fadeout">Promises Held Privately</div>
+            <div class="centered s1-2 fadeout"> Free Enterprise </div>
+        </div>
+
+        <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>           <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>           <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>           <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+            <br><br><br><br>
+
+            <div>
+                <h1> ABOUT  </h1> 
+            </div>
+        </div>
+
+
+
+<br><br><br><br><br>
+
+
+
+
+
+
+
+
+
     <!-- SECTION 3 -->
 <br><br>
-    <h1>SECTION 3 : </h1>
 <br><br><br><br><br><br><br>
 
     <div class="s3">
@@ -78,3 +128,16 @@
             </form>
             <br><br><br><br><br><br>
         </div>
+
+<script>
+    $(document).ready(function(){
+        $(window).scroll(function(){
+        $(".s1-container").css("opacity", 1 - $(window).scrollTop() / $('.s1-container').height() /2 );
+    });
+
+});
+
+</script>
+
+    </body>
+</html>
