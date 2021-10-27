95aa6a82e0cdd209c3071c5f6ccb546da447fa34
JohnHajjar <john.hajjar@isae.edu.lb>
2021-10-27 11:22:00 +0300

    test1

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+    </head>
+    <body>
+
+    <!-- SECTION 3 -->
+<br><br>
+    <h1>SECTION 3 : </h1>
+<br><br><br><br><br><br><br>
+
+    <div class="s3">
+        <div class="s3-1">
+            <img src="imgs/wallpaper-s3-1.jpg" width="90%" height="110%">
+            <h3 class="s3-text1"> SEEKING OPPORTUNITY</h3>
+            <h3 class="s3-text2"> PRESERVING VISION </h3>
+        </div>
+
+        <div class="s3-2">
+            <img src="imgs/wallpaper-s3-2.jpg" width="90%" height="110%">
+            <h3 class="s3-text1"> MITIGATING RISK </h3>
+            <h3 class="s3-text2"> EVALUATING PROSPECTS </h3>
+        </div>
+
+    </div>  
+    <br>
+    <br><br><br><br><br><br><br><br>
+    <h1> SECTION 5 </h1>
+    <br><br><br>
+
+    <div class="s5">
+        <div class="s5-1">
+            <img src="imgs/wallpaper-s5-1.jpg" width="80%">
+            <h3 class="s51-text1"> INVESTOR RELATIONS </h3>
+            <h3 class="s51-text2"> PROPRIETARY INVESTMENTS </h3>
+            <h3 class="s51-text3"> PRIVATE DEALS </h3>
+
+        </div>
+        <div class="s5-2">
+            <img src="imgs/wallpaper-s5-2.jpg" width="80%">
+            <h3 class="s52-text1"> AFFILIATED ENTERPRISE </h3>
+            <h3 class="s52-text2"> CORPORATE VENTURES </h3>
+            <h3 class="s52-text3"> GENERAL SERVICES </h3>
+
+        </div>
+    </div>
+        <div class="s5-3">
+            <img src="imgs/wallpaper-s5-3.jpg" width="95%">
+            <h3 class="s53-text" align=center > OPPORTUNITY IS ALWAYS SOUGHT AFTER</h3>
+        </div>
+
+
+        <br><br><br><br><br>
+        <br><br><br><br><br><br>
+
+        <h1>SECTION 7 : FORM </h1>
+
+
+        <div class="s7">
+            <br>
+            <h2 align=center>Admission Form</h2>
+            <form>
+                <input type="text" placeholder="First & Last Name">
+                <input type="text" placeholder="Email Address">
+                <input type="text" placeholder="Email Address">
+                <input type="text" placeholder="LinkedIn Profile (URL)">
+                <input type="text" placeholder="Phone Number"> <br> 
+                <input type="text" placeholder="Role Type"> <br>
+                    <textarea cols="20" rows="10" placeholder="Your Message"></textarea>
+                <input type="text" placeholder="Chosen Position">
+
+            
+                
+                
+                
+                
+                
+            </form>
+            <br><br><br><br><br><br>
+        </div>
