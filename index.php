b8666bae0f5492a9b3e1a515737ac6ad8d1b331e
John Hajjar <john.hajjar@isae.edu.lb>
2021-12-13 11:25:55 +0200

    john last comm

--- index.php
+++ index.php
@@ -5,127 +5,182 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="stylesheet.css"/>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+  
         <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>    -->
         <script type="text/javascript" src="jquery-comp-3.6.js"></script>
     </head>
     <body>
 
-    <nav> 
+    <section id="navbar">
+    <nav class="navbar py-4 fixed-top navbar-expand-lg navbar-dark">
+      <div class="container">
+        <h1><a class="navbar-brand d-lg-none" href="#">CapB</a></h1>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="row collapse navbar-collapse" id="navbarNavAltMarkup">
+          <ul class="navbar-nav nav">
+            <li class="nav-item me-lg-2 mb-4 mb-lg-0 mt-4 mt-lg-auto">
+              <a href="#">About</a>
+            </li>
+            <span class="mt-auto me-2 d-none d-lg-block"> | </span>
+            <li class="nav-item me-lg-2 mb-4 mb-lg-0 mt-auto">
+              <a href="#">Contact</a>
+            </li>
+            <span class="mt-auto me-2 d-none d-lg-block"> | </span>
+            <li class="nav-item me-lg-auto mb-4 mb-lg-0 mt-auto">
+              <a href="">Careers</a>
+            </li>
+            <h1 class="d-none d-lg-block"><a class="mx-lg-auto" href="#">CapB</a></h1>
+            <li class="nav-item ms-lg-auto me-lg-2 mb-4 mb-lg-0 mt-auto">
+              <a href="#">Finance</a>
+            </li>
+            <span class="mt-auto me-2 d-none d-lg-block"> | </span>
+            <li class="nav-item mt-auto mb-4 mb-lg-0">
+              <a href="">Enterprise</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-    <div class="s1-container ">  <!-- container-flex -->
+  </section>
+    <div class="s1-container ">  
         <div class = "text-Box">
-            <!-- <br><br><br><br> -->
             <div class = "centered s1-1 fadeout">Promises Held Privately</div>
             <div class="centered s1-2 fadeout"> Free Enterprise </div>
         </div>
+<br><br><br><br><br><br>
+          <!-- SECTION 2 -->
+          <div>
+            <div class = "container-fluid about" style="opacity: 1 !important;" >
+            <p class = "head-title" align=center> 
+              ABOUT
+            </p>
 
-        <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>           <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>           <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>           <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>
-            <br><br><br><br>
+          <p class = "details">A multifaceted, multipurpose and multinational 
+            investment office.
+             </p>
+          <p class = "details">
+            <span class = "key-word">CapB</span>is a privately-held enterprise that ventures 
+            into investment opportunities, hosts an online 
+            services-based platform, and sponsors its very own 
+            research initiative. For all that is deemed 
+            appropriate, we discover, promote and finance 
+            hopeful ventures that serve the interests of our 
+            clients, sponsors and associates.
+          </p>
 
-            <div>
-                <h1> ABOUT  </h1> 
+          <p class = "details">A large portion of our activities strictly pertain to 
+            matters of Investment Banking, Investment 
+            Research and Wealth & Asset Management.
+          </p>
+
+        <p class = "details">
+            All entities affiliated with <span class = "key-word">CapB</span> are within the scope 
+            of operation and administration of the privately held firm. These include: <span class = "key-word">Sovereign Wealth 
+            Reserve, BBTC</span> and <span class = "key-word">iO Banking Solutions</span>.
+        </p>
+        
+    </div>
+      </div>
+        </div>
+
+    <!-- SECTION 3  --> 
+        <div class="s3">
+            <div class="s3div s3-1">
+            <p class="text-ts3-1 fontsize-bs"> SEEKING OPPORTUNITY</p>
+            <p  class="text-ts3-2 fontsize-bs"> PRESERVING VISION</p>
+            </div>
+            <div class="s3div s3-2">
+            <p class="text-ts3-1 fontsize-bs"> MITIGATING RISK </p>
+            <p  class="text-ts3-2 fontsize-bs">  EVALUATING PROSPECTS</p>
             </div>
         </div>
 
-
-
-<br><br><br><br><br>
-
-
-
-
-
-
-
-
-
- 
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
- <!-- SECTION 3  --> 
-    <div class="s3">
-        <div class="s3div s3-1">
-        <p class="text-ts3-1"> SEEKING OPPORTUNITY</p>
-        <p  class="text-ts3-2"> PRESERVING VISION</p>
+    <!-- SECTION 4 -->
+    <section id="financeEnterprise">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 text-center finance">
+          <h1>FINANCE</h1>
+          <img src="imgs/Finance.png" alt="finance-img">
+          <p>A</p>
         </div>
-        <div class="s3div s3-2">
-        <p class="text-ts3-1"> MITIGATING RISK </p>
-        <p  class="text-ts3-2">  EVALUATING PROSPECTS</p>
+        <span class="vertical-line d-none d-lg-block"></span>
+        <div class="col-lg-6 text-center enterprise">
+          <img src="imgs/Enterprise.png" alt="finance-img">
+          <h1>ENTERPRISE</h1>
+          <p>A multifaceted, multipurpose and multinational
+            investment office.</p>
+          <p><strong>CapB</strong> is a privately-held enterprise that ventures
+            into investment opportunities, hosts an online
+            services-based platform, and sponsors its very own
+            research initiative. For all that is deemed
+            appropriate, we discover, promote and finance
+            hopeful ventures that serve the interests of our
+            clients, sponsors and associates.</p>
+          <p>A large portion of our activities strictly pertain to
+            matters of Investment Banking, Investment
+            Research and Wealth & Asset Management.</p>
+          <p>All entities affiliated with <strong>CapB</strong> are within the scope
+            of operation and administration of the privatelyheld
+            firm. These include: <strong>Sovereign Wealth
+              Reserve</strong>, <strong>BBTC</strong> and <strong>iO Banking Solutions</strong>.</p>
         </div>
+      </div>
     </div>
+  </section>
 
-
-    <br>
-    <br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br>
-    <!-- SECTION 5 -->
+  <!-- SECTION 5 -->
     <table class="s5-table">
-        <tr style=" border: 2px solid red;">
-            <td class="s5-td1" style=" border: 2px solid red;">
-                <p>INVESTOR RELATIONS</p><br><br class="br-big-screen">
+        <tr>
+            <td class="s5-td1 ">
+                <p> INVESTOR RELATIONS</p><br><br class="br-big-screen">
                 <p>PROPRIETARY INVESTMENT</p><br><br class="br-big-screen">
                 <p>PRIVATE DEALS</p>
             </td>
-            <td style="width:10%"></td>
-            <td class="s5-td2" style="border: 2px solid red;">
+            <td style="width:4%;"></td>
+            <td class="s5-td2">
                 <p>AFFILIATED ENTERPRISE</p><br><br class="br-big-screen">
                 <p>CORPORATE VENTURES</p><br><br class="br-big-screen">
                 <p>GENERAL SERVICES</p>
             </td>
         </tr>
-        <tr class="s5-tr2" style=" border-top: 15px solid white;">
-            <td class="s5-td3" colspan="3" style="border: 2px solid red;"> 
+        <tr class="s5-tr2" style="border-top: 15px solid white;">
+            <td class="s5-td3" colspan="3"> 
            <br><p>OPPORTUNITY IS ALWAYS SOUGHT AFTER</p>
             </td>
         </tr>
     </table>
 
+    <!-- SECTION 6 -->
+    <div class="s6">
+    <p class ="s6-title" align=center>PROFESSIONAL CAREERS</p>
+    <div class="s6">
+    <img src="imgs/icon3.png" width="40%">
+    </div>
 
+    </div>
 
-    
-    <br>
-    <br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br>
-    <br><br><br><br><br><br><br><br>
-
-
-        <h1>SECTION 7 : FORM </h1>
-
-
-        <div class="s7">
+          <div class="s7">
             <br>
-            <h2 align=center>Admission Form</h2>
-            <form>
-                <input type="text" placeholder="First & Last Name">
-                <input type="text" placeholder="Email Address">
-                <input type="text" placeholder="Email Address">
-                <input type="text" placeholder="LinkedIn Profile (URL)">
-                <input type="text" placeholder="Phone Number"> <br> 
-                <input type="text" placeholder="Role Type"> <br>
-                <input type="text" placeholder="Chosen Position">
+            <h2 align=center name = "form">Admission Form</h2>
+            <form class="s7form" method = "post">
+                <div class="s7-left" >
+                    <input type="text" name="name" placeholder="First & Last Name"> <br>
+                    <input type="text" name="email" placeholder="Email Address"> <br>
+                    <input type="text" name="pnumber" placeholder="Phone Number"> <br> 
+                    <input type="text" name="roletype" placeholder="Role Type"> <br>
+                    <input type="text" name="position" placeholder="Chosen Position"><br>
+                </div>
+                <div class="s7-right">
+                    <input type="text" name="linkedin" placeholder="LinkedIn Profile (URL)"> <br>
+                    <textarea class = "message" name="usermessage" cols="22.5" rows="10" placeholder="Your Message"></textarea>
+                 </div>
 
-                <textarea cols="20" rows="10" placeholder="Your Message"></textarea>
+      
+                <button type="submit" class="s7-apply-btn" name="submit">Apply</button>
                 
                 
                 
@@ -133,10 +188,72 @@
             <br><br><br><br><br><br>
         </div>
 
+
+
+
+    <section class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 text-center text-lg-start left-footer">
+          <p>General Inquiry</p>
+          <h6><strong>Phone: +961 81-343-722</strong></h6>
+          <h6><strong>Email: <a class="afooter" href="mailto:info@capb.me">info@capb.me</a></strong></h6>
+            <ul>
+              <li>
+                <a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+              </li>
+            </ul>
+        </div>
+        <div class="col-lg-4 text-center middle-footer">
+          <p><u><strong>B</strong></u></p>
+        </div>
+        <div class="col-lg-4 text-center text-lg-end pt-md-4 py-lg-0 right-footer">
+          <h4>Copyright © 2021 CapB</h4>
+          <p><em>Free Enterprise</em></p>
+          <strong>
+            <p>Bad Idriss, Beirut, Lebanon</p>
+          </strong>
+        </div>
+      </div>
+    </div>
+  </section>
+ 
+<?php
+//   if(isset($_POST['submit'])){
+//         $to = "john-hajjar@hotmail.com"; // Admin mail
+//     $from = $_POST['email']; // this is the sender's Email address
+//     $name = $_POST['name'];
+//     $subject = "Admission form submission";
+//     $subject2 = "Copy of your form submission";
+//     $message = $name . " " . " wrote the following:" . "\n\n" . $_POST['usermessage'];
+//     $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['usermessage'];
+
+//     $headers = "From:" . $from;
+//     $headers2 = "From:" . $to;
+//     mail($to,$subject,$message,$headers);
+//     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+//     echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+//     }
+?> 
+
+
+
 <script>
     $(document).ready(function(){
         $(window).scroll(function(){
         $(".s1-container").css("opacity", 1 - $(window).scrollTop() / $('.s1-container').height() /2 );
+
+
+        $(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > 200);
+    });
+  });
+
     });
 
 });
