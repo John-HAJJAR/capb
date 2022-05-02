87dcc86c4958607fa7ee4b421fee1f5f8f191453
wissamnns <72502383+wissamnns@users.noreply.github.com>
2022-05-02 17:03:18 +0300

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -13,14 +13,14 @@
     <section id="navbar">
     <nav class="navbar py-4 fixed-top navbar-expand-lg navbar-dark">
       <div class="container">
-        <h1><a class="navbar-brand d-lg-none capb" href="#">CapB</a></h1>
+        <h1><a class="navbar-brand d-lg-none capb" href="../index.php">CapB</a></h1>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="row collapse navbar-collapse" id="navbarNavAltMarkup">
           <ul class="navbar-nav nav">
             <li class="nav-item me-lg-2 mb-4 mb-lg-0 mt-4 mt-lg-auto">
-              <a href="/capb#about">About</a>
+              <a href="../index.php">About</a>
             </li>
             <span class="mt-auto me-2 d-none d-lg-block nav-item"> | </span>
             <li class="nav-item me-lg-2 mb-4 mb-lg-0 mt-auto">
@@ -28,7 +28,7 @@
             </li>
             <span class="mt-auto me-2 d-none d-lg-block nav-item"> | </span>
             <li class="nav-item me-lg-auto mb-4 mb-lg-0 mt-auto">
-              <a href="">Careers</a>
+              <a href="../index.php/#s5">Careers</a>
             </li>
             <h1 class="d-none capb123 d-lg-block"><a class="mx-lg-auto" href="#">CapB</a></h1>
             <li class="nav-item ms-lg-auto me-lg-2 mb-4 mb-lg-0 mt-auto">
