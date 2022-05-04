c52ee6a16bf51dc593bdff3c15c45f5227f2516c
wissamnns <72502383+wissamnns@users.noreply.github.com>
2022-05-04 14:27:42 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 $errors= [];
-$conn = new mysqli('localhost', 'id18868820_capbweb', '', 'id18868820_capb');
+$conn = new mysqli('localhost', 'id18868820_capbweb', 'EQ_9gIwIV(PJ<{\|', 'id18868820_capb');
 
 error_reporting(0);
 
